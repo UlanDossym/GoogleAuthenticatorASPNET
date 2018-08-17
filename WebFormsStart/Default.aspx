0d6065aa553c2797150fad2ee7ca528d89d95497
Ulan Dossym <udos7853@uni.sydney.edu.com>
2018-08-17 22:38:40 +0600

    initial commit

---
+++ WebFormsStart/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <asp:Image ID="imgQRcode" runat="server" />
+
+    <p></p>
+    <div>
+        <asp:Label ID="lblSecretCode" runat="server"></asp:Label>
+    </div>
+    <form id="form1" runat="server">
+        <div>
+            Name:
+            <asp:TextBox ID="NameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+            <br />
+            <br />
+            <asp:Label ID="OutputLabel" runat="server"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebFormsStart/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using Google.Authenticator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TwoFactorAuthenticator tfa = new TwoFactorAuthenticator();
+        var setupInfo = tfa.GenerateSetupCode("MyApp", "user@example.com", "SuperSecretKeyGoesHere", 300, 300);
+        var setupInfo2 = tfa.GetCurrentPIN("SuperSecretKeyGoesHere");
+        lblSecretCode.Text = setupInfo2;
+        string qrCodeImageUrl = setupInfo.QrCodeSetupImageUrl;
+        string manualEntrySetupCode = setupInfo.ManualEntryKey;
+        bool isCorrectPIN = tfa.ValidateTwoFactorPIN("SuperSecretKeyGoesHere", "123456");
+        imgQRcode.ImageUrl = qrCodeImageUrl;
+        imgQRcode.Width = 300;
+        imgQRcode.Height = 300;
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        OutputLabel.Text = NameTextBox.Text;
+    }
+}
